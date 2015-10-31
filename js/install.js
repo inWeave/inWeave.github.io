ec3be5a713ebc4d824550016025081f57f2ff0b7
Havelka Martin <havelkam@avast.com>
2015-10-31 22:35:54 +0100

    Added code to trigger extension installs and extension states handling.

---
+++ js/install.js
@@ -0,0 +1,69 @@
+/**
+ * IW code to mamage IW extension installation states.
+ * Shows/hides elements based on its classes according installation states:
+ *
+ *   .iw-not-supported-browser - page is open in unsupported (Chrome) browser
+ *   .iw-can-install-extension - extension can be installed, provide the install trigger
+ *   .iw-extension-installed - extension is already installed in the browser
+ *   .iw-installation-success -
+ *   .iw-installation-failure -
+ *
+ * Mark node with class
+ *   .iw-click-install - to let the code attach event to trigger extension installation
+ */
+function modifyClass(nodeClass, disp) {
+  var nds = document.querySelectorAll(nodeClass);
+  if (nds && nds.length > 0) {
+    Array.prototype.forEach.call(nds, disp);
+  }
+}
+
+function modifyClasses(nodeClasses, disp) {
+  nodeClasses.forEach(function(nodeClass) {
+    modifyClass(nodeClass, disp);
+  })
+}
+
+function setDisplay(nd, val) { nd.style.display = val; }
+function dispHide(nd) { setDisplay(nd, 'none'); }
+function dispShow(nd) { setDisplay(nd, 'block'); }
+
+function initInstallButton (buttNd) {
+  console.log('add install', buttNd);
+  buttNd.addEventListener("click", function(e) {
+    if (chrome) {
+      chrome.webstore.install("https://chrome.google.com/webstore/detail/kkphiaafmfbkggbicfnggaaikbljcafg",
+        function successCallback() {
+          modifyClass('.iw-installation-success', dispShow);
+          modifyClasses(['.iw-can-install-extension', '.iw-installation-failure'], dispHide);
+        },
+        function failureCallback() {
+          modifyClass('.iw-installation-failure', dispShow);
+        }
+      );
+    }
+  });
+}
+
+function isIWinstalled() {
+  var installNodeMarker = document.querySelector('#IW-is-installed');
+  return (installNodeMarker);
+}
+
+document.addEventListener("DOMContentLoaded", function init() {
+  if (typeof chrome != 'undefined') {
+    if (isIWinstalled()) {
+      modifyClass('.iw-extension-installed', dispShow);
+      modifyClasses(['.iw-can-install-extension', '.iw-not-supported-browser'], dispHide);
+    } else {
+      modifyClass('.iw-click-install', initInstallButton);
+      modifyClass('.iw-can-install-extension', dispShow);
+      modifyClasses(['.iw-extension-installed', '.iw-not-supported-browser'], dispHide);
+    }
+  } else {
+    // unsupported browser
+    modifyClass('.iw-not-supported-browser', dispShow);
+    modifyClasses(['.iw-can-install-extension', '.iw-extension-installed'], dispHide);
+  }
+  modifyClasses(['.iw-installation-success', '.iw-installation-failure'], dispHide);
+}, false);
